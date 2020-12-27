65bc3c0edb10884d97a014c7e6d405bc1b714858
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2020-12-27 11:30:28 -0500

    server setup

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// import the needed node_modules.
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+express()
+  // Below are methods that are included in express(). We chain them for convenience.
+  // --------------------------------------------------------------------------------
+
+  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+  .use(morgan("tiny"))
+  .use(bodyParser.json())
+
+  // Any requests for static files will go into the public folder
+  .use(express.static("public"))
+
+  // Nothing to modify above this line
+  // ---------------------------------
+  // add new endpoints here 👇
+  
+
+  // add new endpoints here ☝️
+  // ---------------------------------
+  // Nothing to modify below this line
+
+  // this is our catch all endpoint.
+
+
+  // Node spins up our server and sets it to listen on port 8000.
+  .listen(8000, () => console.log(`Listening on port 8000`));
